2c795f4d9b3ad49f55a6f22656d66a81519d10af
rhyho <jamielewisainsworth@gmail.com>
2021-09-01 02:48:31 +0100

    Update bot.py
    Added member welcome message

--- bot.py
+++ bot.py
@@ -9,11 +9,18 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
-@client.event
+@client.event #confirms the bot has connected to discord
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
 @client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to the server, please read the rules and react to select your role.'
+    )
+
+@client.event #event for messages and commands
 async def on_message(message):
     if message.author == client.user:
         return
@@ -23,5 +30,6 @@ async def on_message(message):
 
     if message.content.startswith('!help'): #Provides all commands
         await message.channel.send('temp placeholder') #feedsback list of commands
+
 
 client.run(TOKEN)
